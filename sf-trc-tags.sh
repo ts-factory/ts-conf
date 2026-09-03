Xilinx-CNS/cns-ts-conf

commit 878f35ba0e4779625356aaec1022a85f94e12e46

sf-trc-tags: set FW variant specified in dev args
Make sure the getcaps command returns capabilities associated with the FW
variant the DPDK driver is going to set up during test execution.

Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Ivan Malov <ivan.malov@arknetworks.am>

sf-trc-tags.sh
@@ -243,6 +243,24 @@ else
     cmdclient_transport="tlp=${pci_dbsf}"
 fi
 
+# Check if device arguments override the FW variant
+IFS=,
+for arg in ${TE_IUT_DEV_ARGS:-${TE_DPDK_DEV_ARGS}} ; do
+    case "${arg}" in
+        fw_variant=full-feature)
+            rx_dpcpu_expected="full-featured"
+            tx_dpcpu_expected="full-featured"
+            do_cmdclient "drv_detach;drv_attach full_featured;quit"
+            ;;
+        fw_variant=ultra-low-latency)
+            rx_dpcpu_expected="low-latency"
+            tx_dpcpu_expected="low-latency"
+            do_cmdclient "drv_detach;drv_attach low_latency;quit"
+            ;;
+    esac
+done
+unset IFS
+
 out_version="$(do_cmdclient "version;quit")"
 out_caps="$(do_cmdclient "getcaps;quit")"
 
@@ -284,6 +302,11 @@ test -z "${rx_dpcpu}" -o "${rx_dpcpu}" = "<unknown>" ||
 tx_dpcpu="$(echo "${out_caps}" | grep 'TX DPCPU' | sed 's/.*=> //' | tr ' ' -)"
 test -z "${tx_dpcpu}" -o "${tx_dpcpu}" = "<unknown>" ||
     tags+=("tx-${tx_dpcpu}")
+
+if test -n "${rx_dpcpu_expected}" -a "${rx_dpcpu}" != "${rx_dpcpu_expected}" -o \
+        -n "${tx_dpcpu_expected}" -a "${tx_dpcpu}" != "${tx_dpcpu_expected}"; then
+    echo "Failed to set the FW variant specified in devargs" >&2
+fi
 
 # Add tags based on firmware capabilities.
 echo "${out_caps}" | grep -q '^ *Supports TX VLAN insertion *: *no$' &&

ts-factory/ts-conf

commit 359c6b83ed6c7300ee848b1aa1968831d4a7eac4

sf-trc-tags.sh: redirect driver attach log to stderr
Make sure the final TRC tags are not polluted by extra output from these
commands.

Fixes: 878f35ba0e47 ("sf-trc-tags: set FW variant specified in dev args")
Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Daniil Byshenko <Daniil.Byshenko@arknetworks.am>

sf-trc-tags.sh
@@ -250,12 +250,12 @@ for arg in ${TE_IUT_DEV_ARGS:-${TE_DPDK_DEV_ARGS}} ; do
         fw_variant=full-feature)
             rx_dpcpu_expected="full-featured"
             tx_dpcpu_expected="full-featured"
-            do_cmdclient "drv_detach;drv_attach full_featured;quit"
+            do_cmdclient "drv_detach;drv_attach full_featured;quit" >&2
             ;;
         fw_variant=ultra-low-latency)
             rx_dpcpu_expected="low-latency"
             tx_dpcpu_expected="low-latency"
-            do_cmdclient "drv_detach;drv_attach low_latency;quit"
+            do_cmdclient "drv_detach;drv_attach low_latency;quit" >&2
             ;;
     esac
 done